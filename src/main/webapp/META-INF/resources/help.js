2967a44aff9163f9aabbc951e98430d603bcf8c1
Everton <everton.nrb@hotmail.com>
2017-10-31 09:05:29 -0300

    Front + Angular

--- src/main/webapp/META-INF/resources/help.js
+++ src/main/webapp/META-INF/resources/help.js
@@ -1,11 +1,78 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 angular.module("HelpApp",[]);
-    controller("ChamadoController",function (){
+        
+        value('urlBase','http://localhost:8080/crud_rest/rest/')
+        
+        controller("ChamadoController",function ($http,urlBase){
         var self = this;
-        self.usuario = 'Everton Ribeiro';
+        
+            self.usuario = 'Everton Ribeiro';
+            
+            self.chamados = []; 
+            
+            self.chamado = undefined;
+            
+            self.salvar = function(){
+                var metodo = 'POST';
+                if(self.chamado.id){
+                    metodo = 'PUT';
+                }
+                
+                $http({
+                    method: metodo,
+                    url: urlBase+'chamado/',
+                    data:self.chamado
+                }) .then (function successCallBack(response){
+                  self.atualizarTabela();  
+                },function errorCallBack(response){
+                   self.ocorreuErro(); 
+                });
+            };
+            
+            self.novo = function (){
+              self.chamado = {};  
+            };
+            
+            self.alterar = function(chamado){
+                this.chamado = chamado;
+            };
+            
+            self.deletar = function(chamado){
+                self.chamado = chamado;
+                
+                $http({
+                    method: 'DELETE',
+                    url: urlBase+'chamado/'+self.chamado.id+"/"
+                }) .then (function successCallBack(response){
+                  self.atualizarTabela();  
+                },function errorCallBack(response){
+                   self.ocorreuErro(); 
+                });
+                
+            };
+            
+            self.concluir = function(){
+                alert("TODO");
+            };
+            
+            self.ocorreuErro = function (){
+                alert("Erro inesperado");
+            };
+            
+            self.atualizarTabela = function (){
+                   $http({
+                    method: 'GET',
+                    url: urlBase+'chamados/'
+                }) .then (function successCallBack(response){
+                    self.chamados = response.data;
+                    self.chamado = undefined;
+                },function errorCallBack(response){
+                   self.ocorreuErro(); 
+                });
+            };
+            
+            self.activate = function () {
+                self.atualizarTabela();
+            };
+            self.activate();
     });
